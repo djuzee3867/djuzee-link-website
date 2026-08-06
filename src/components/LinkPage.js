5cdef20179b90089d62a933a37c4c567c7cd5364
djuzee_ <natchaphol.3867@gmail.com>
2026-08-06 17:03:18 +0700

    Update LinkPage.js

--- src/components/LinkPage.js
+++ src/components/LinkPage.js
@@ -103,7 +103,8 @@ export default function LinkPage() {
     { title: "Schedule", desc: "Timetable and appointment tracking", url: "/Schedule", icon: <CalendarIcon /> },
     { title: "QR Code", desc: "Generate and manage quick links", url: "/qr", icon: <QrIcon /> },
     { title: "Foodwaste", desc: "The Past of Research for KKR-NCST", url: "/foodwaste", icon: <ChartIcon /> },
-    { title: "Wheel Spin", desc: "Wheel Spin", url: "/spin", icon: <GridIcon /> },
+    { title: "drive parade", desc: "Wheel Spin", url: "/https://drive.google.com/drive/folders/1HZUwuWTUFxcOfmRRYAF1lRl8AQNJVKUK?hl=th", icon: <GridIcon /> },
+
   ];
 
   if (!mounted) return null;
